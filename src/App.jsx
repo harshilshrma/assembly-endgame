0dfc962f3c61d35ae79b06937a813979f8cdf48f
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-12-11 15:36:03 +0530

    basic layout + status section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ function App() {
 
   return (
     <main>
-      Game goes here
+      <header>
+        <h1>Assembly Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+
+      <section className="game-status">
+        <h3>Game over!</h3>
+        <p>You lose! Better start learning Assembly 😭</p>
+      </section>
+
+      <section className="languages-list">
+        
+      </section>
     </main>
   )
 }
